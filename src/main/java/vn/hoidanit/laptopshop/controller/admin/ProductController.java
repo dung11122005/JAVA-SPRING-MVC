de54be6f41d6a7e873a75b03a41b5123deb57a47
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-07-27 00:14:12 +0700

    1

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -1,16 +1,40 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UploadService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+    private final UploadService uploadService;
+
+    public ProductController(
+            ProductService productService,
+            UploadService uploadService) {
+        this.productService = productService;
+        this.uploadService = uploadService;
+    }
+
     @GetMapping("/admin/product")
-    public String getProduct() {
+    public String getProduct(Model model) {
+        List<Product> prs = this.productService.fetchProducts();
+        model.addAttribute("products", prs);
         return "/admin/product/show";
     }
 
@@ -19,4 +43,21 @@ public class ProductController {
         model.addAttribute("newProduct", new Product());
         return "/admin/product/create";
     }
+
+    @PostMapping("/admin/product/create")
+    public String handleCreateProduct(Model model,
+            @ModelAttribute("newProduct") @Valid Product pr,
+            BindingResult newProductBindingResult,
+            @RequestParam("hoidanitFile") MultipartFile file) {
+        if (newProductBindingResult.hasErrors()) {
+            return "admin/product/create";
+        }
+
+        String image = this.uploadService.handleSaveUploadFile(file, "product");
+        pr.setImage(image);
+
+        this.productService.createProduct(pr);
+        return "redirect:/admin/product";
+    }
+
 }
