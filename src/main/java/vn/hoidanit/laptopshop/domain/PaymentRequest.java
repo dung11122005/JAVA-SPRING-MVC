e25d1a70a2737f8a90689192bc7a570c5dc80207
dung11122005 <dungakaishi900@gmail.com>
2025-07-18 23:21:54 +0700

    1

--- src/main/java/vn/hoidanit/laptopshop/domain/PaymentRequest.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/PaymentRequest.java
@@ -11,4 +11,45 @@ public class PaymentRequest {
     private String orderInfo;
     private String requestId;
     private String extraData;
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderInfo() {
+        return orderInfo;
+    }
+
+    public void setOrderInfo(String orderInfo) {
+        this.orderInfo = orderInfo;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getExtraData() {
+        return extraData;
+    }
+
+    public void setExtraData(String extraData) {
+        this.extraData = extraData;
+    }
+
 }
